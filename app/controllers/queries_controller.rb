b7f696f9990cb073ce4fa3662f3b7de83c2a81a5
Mohit Israni <mohitisrani@hotmail.com>
2018-04-20 04:50:21 -0400

    Working query

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,20 +1,19 @@
 class QueriesController < ApplicationController
 
 
-	def self.index
+	def index
 		ActiveRecord::Base.establish_connection()
 		"Successfully Connected to DataBase"
 	end
 
-	def self.accidents_count
-		count = ActiveRecord::Base.connection.exec_query("SELECT COUNT(*) FROM ACCIDENTS")[0]["count(*)"];
-		count
-	end
-
-	# def self.accidents_count
-	# 	count = ActiveRecord::Base.connection.exec_query("SELECT COUNT(*) FROM ACCIDENTS")[0]["count(*)"];
-	# 	count
+	# def query
+	# 	@query = ActiveRecord::Base.connection.exec_query("SELECT COUNT(*) FROM ACCIDENTS")[0]["count(*)"];
 	# end
+
+	def query
+		@query = ActiveRecord::Base.connection.exec_query("SELECT accident_index FROM ACCIDENTS where rownum<=5")
+		
+	end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  root 'posts#index'
+  root 'queries#index'
 
-  get 'count' =>'queries#index'
+  get 'count' =>'queries#query'
 
 
 end
